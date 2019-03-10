aed224129234d7625da69baeb8c72ab9e29c3be5
Walkinged <43610419+Walkinged@users.noreply.github.com>
2019-03-10 15:29:29 +0000

    Add files via upload
    This project can be useful when creating lock screens, requiring usernames and/ or passwords

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Form2 secondForm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            secondForm = new Form2(); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            secondForm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            secondForm.passname = textBox1.Text;
+            secondForm.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        private string name;
+
+        public string passname
+        {
+            get { return name; }
+
+            set
+            {
+                name = value;
+                label1.Text = name;
+            }
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+    }
+}
